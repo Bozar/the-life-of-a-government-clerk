98ad5028beca4ffbe651de335d655f6292303ac2
bozar <5583771+Bozar@users.noreply.github.com>
2024-09-10 13:59:54 +0800

    Cleanify wizard key functions for presentation.

--- scene/main/wizard_mode.gd
+++ scene/main/wizard_mode.gd
@@ -2,57 +2,50 @@ class_name WizardMode
 extends Node2D
 
 
+var _counter: int
+
+
 func handle_input(input_tag: StringName) -> void:
     _test(input_tag)
 
 
 func _test(input_tag: StringName) -> void:
+    var ref_cart: Cart = get_node("../Cart")
     var pc: Sprite2D = SpriteState.get_sprites_by_sub_tag(SubTag.PC)[0]
-    var carts: Array
     var cart: Sprite2D
-    var cart_state: CartState
+    var state: CartState
 
     match input_tag:
         InputTag.WIZARD_1:
-            get_node("../Cart").add_cart(7)
+            ref_cart.add_cart(GameData.MIN_CART)
         InputTag.WIZARD_2:
-            carts = SpriteState.get_sprites_by_sub_tag(SubTag.CART)
-            if carts.size() > 6:
-                carts.resize(6)
-            for i in range(0, carts.size()):
-                cart = carts[i]
-                cart_state = get_node("../Cart").get_state(cart)
-                match i:
-                    0:
-                        cart_state.item_tag = SubTag.ATLAS
-                        cart_state.load_factor = i * 20
-                    1:
-                        cart_state.item_tag = SubTag.BOOK
-                        cart_state.load_factor = i * 20
-                    2:
-                        cart_state.item_tag = SubTag.CUP
-                        cart_state.load_factor = i * 20
-                    3:
-                        cart_state.item_tag = SubTag.DOCUMENT
-                        cart_state.load_factor = i * 20
-                    4:
-                        cart_state.item_tag = SubTag.ATLAS
-                        cart_state.is_detached = true
-                        cart_state.load_factor = i * 20
-                    5:
-                        cart_state.item_tag = SubTag.BOOK
-                        cart_state.load_factor = 120
+            cart = ref_cart.get_last_slot(pc)
+            if cart == null:
+                return
+            state = ref_cart.get_state(cart)
+            match _counter:
+                0:
+                    state.item_tag = SubTag.ATLAS
+                1:
+                    state.item_tag = SubTag.ATLAS
+                2:
+                    state.item_tag = SubTag.BOOK
+                3:
+                    state.item_tag = SubTag.CUP
+                4:
+                    state.item_tag = SubTag.DOCUMENT
+                5:
+                    state.item_tag = SubTag.ATLAS
+                6, _:
+                    _counter = 0
+                    return
+            state.load_factor = 100 - _counter * 20
+            _counter += 1
         InputTag.WIZARD_3:
-            carts = SpriteState.get_sprites_by_sub_tag(SubTag.CART)
-            get_node("../Cart")._remove_cart(pc, carts[0])
+            cart = ref_cart.get_first_item(pc)
+            if cart == null:
+                return
+            state = ref_cart.get_state(cart)
+            state.item_tag = SubTag.CART
         InputTag.WIZARD_4:
-            cart = get_node("../Cart").get_last_slot(pc)
-            if cart != null:
-                cart_state = get_node("../Cart").get_state(cart)
-                cart_state.load_factor = 120
-            cart = get_node("../Cart").get_last_slot(pc)
-            if cart != null:
-                cart_state = get_node("../Cart").get_state(cart)
-                cart_state.item_tag = SubTag.CUP
-        InputTag.WIZARD_5:
-            print(get_node("../Cart").clean_cart(pc))
+            print(ref_cart.clean_cart(pc))

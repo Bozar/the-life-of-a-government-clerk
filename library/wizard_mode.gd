b5e60ea04b5302877500543bee5ef4100247456c
bozar <5583771+Bozar@users.noreply.github.com>
2025-05-30 10:25:33 +0800

    Add more wizard keys.

--- library/wizard_mode.gd
+++ library/wizard_mode.gd
@@ -2,30 +2,37 @@ class_name WizardMode
 
 
 static func handle_input(input_tag: StringName) -> void:
-	_test(input_tag)
-
-
-static func _test(input_tag: StringName) -> void:
 	var dh := NodeHub.ref_DataHub
 
 	match input_tag:
 		InputTag.WIZARD_1:
-			Cart.add_cart(GameData.ADD_CART, dh.linked_cart_state)
-
-		InputTag.WIZARD_2:
-			dh.cash += 1
-
-		InputTag.WIZARD_3:
 			Cart.clean_cart(dh.pc, dh.linked_cart_state)
 
+		InputTag.WIZARD_2:
+			_reset_cooldown()
+
+		InputTag.WIZARD_3:
+			dh.cash += 1
+
 		InputTag.WIZARD_4:
-			var sprite: Sprite2D =	Cart.get_last_slot(
-					dh.pc, dh.linked_cart_state
-			)
-			var state: CartState
-			if sprite != null:
-				state = Cart.get_state(
-						sprite, dh.linked_cart_state
-				)
-				state.item_tag = SubTag.DOCUMENT
+			Cart.add_cart(GameData.ADD_CART, dh.linked_cart_state)
+
+		InputTag.WIZARD_5:
+			_add_document()
+
+
+static func _add_document() -> void:
+	var dh := NodeHub.ref_DataHub
+	var sprite: Sprite2D =	Cart.get_last_slot(dh.pc, dh.linked_cart_state)
+	var state: CartState
+
+	if sprite == null:
+		return
+	state = Cart.get_state(sprite, dh.linked_cart_state)
+	state.item_tag = SubTag.DOCUMENT
+
+
+static func _reset_cooldown() -> void:
+	for i: RawFileState in NodeHub.ref_DataHub.raw_file_states:
+		i.cooldown = 0
 
